e7c6baddbc634180737b2fba46886ce8795139ba
dhyanshah12 <dhyanshah70@gmail.com>
2026-08-18 12:52:02 +0530

    add

--- app.js
+++ app.js
@@ -12,22 +12,68 @@
 // var greet = employees.setUser(12,"raj")
 // console.log(greet)
 
-const express = require("express")
+const express = require("express");
+const { data } = require("./employee");
 
 const app = express()
 
+
+// app.get("/usertable",(req,res)=>{
+//     res.set("content-type","text/html");
+//     res.send(`<table border = 1>
+//             <tr>
+//                 <th>id</th>
+//                 <th>name</th>
+//                 <th>age</th>
+//             </tr>
+//             ${users.map(u => `
+//             <tr>
+//                 <td>${u.id}</td>
+//                 <td>${u.name}</td>
+//                 <td>${u.age}</td>
+//             </tr>
+//             `).join(' ')}
+//         </table>
+//         `)
+// })
+
+const userRoutes = require("./src/Routes/UserRoutes")
+app.use("/user",userRoutes)
+
+const emproutes = require("./src/Routes/EmployeeRoutes")
+app.use("/emp",emproutes)
+
+
+
 const PORT = 3000
 app.listen(PORT,()=>{
-    console.log(`server starteed on Port ${PORT}`);
+    console.log(`server started on Port ${PORT}`);
     
 })
 
 
-app.get("/test",(req,res)=>{
-    console.log("test api called");
-    res.send("test api send")//string
-})
+// app.get("/test",(req,res)=>{
+//     console.log("test api called");
+//     res.send("test api send")//string
+// })
 
-app.get("/user",(req,res)=>{
-    res.json({id:1,name:"Amit"})
-})
+// app.get("/user",(req,res)=>{
+//     res.json({id:1,name:"Amit"})
+// })
+
+
+// var users = [
+
+//     {id:1,name:"amit",age:21},
+//     {id:2,name:"raj",age:22},
+//     {id:3,name:"parth",age:23},
+//     {id:4,name:"akshit",age:24}   
+// ]
+
+// app.get("/users",(req,res)=>{
+
+//         res.json({message:"data fetched succesfully",data:users})
+// })
+
+
+
