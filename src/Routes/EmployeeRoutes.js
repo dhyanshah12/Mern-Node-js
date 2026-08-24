aae3908026e4bc797c724e62f998b289c1837edc
dhyanshah12 <dhyanshah70@gmail.com>
2026-08-24 06:43:51 +0530

    find by id

--- src/Routes/EmployeeRoutes.js
+++ src/Routes/EmployeeRoutes.js
@@ -1,8 +1,10 @@
 const router = require("express").Router()
 
 const EmployeeController = require("../Controllers/EmployeeController")
+const EmpModel = require("../models/EmpModel")
 
 router.get("/employee/:name/:company",EmployeeController.getEmp)
 router.get("/emps",EmployeeController.getEmp)
+router.get("/searhuser",EmployeeController.searchUser)
 
 module.exports = router
--- src/Routes/UserRoutes.js
+++ src/Routes/UserRoutes.js
@@ -4,5 +4,5 @@ const userController = require("../Controllers/UserController")
 
 router.get("/users",userController.getAllUsers)
 router.get("/user/:id",userController.getUserById)
-
+router.get("/searchUser",userController.searchUser)
 module.exports = router
