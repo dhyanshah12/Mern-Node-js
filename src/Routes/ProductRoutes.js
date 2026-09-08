d4c689b29ed57884409d1f31772be4517cdff7f6
dhyanshah12 <dhyanshah70@gmail.com>
2026-09-08 17:23:41 +0530

    update fields

--- src/Routes/ProductRoutes.js
+++ src/Routes/ProductRoutes.js
@@ -1,7 +1,10 @@
 const router = require("express").Router()
 const productController = require("../Controllers/ProductController")
+const zodeMiddleware = require("../middlewares/ZodeMiddleWare")
+const productValidationschema = require("../validationschemas/UserValidationSchemas")
 
-router.post("/",productController.createProduct)
+// router.post("/",productController.createProduct)
+router.post("/",zodeMiddleware(productValidationschema),productController.createProduct)
 router.get("/",productController.getallProducts)
 
 router.put("/updatestock",productController.updateStockStatus)
--- src/Routes/UserRoutes.js
+++ src/Routes/UserRoutes.js
@@ -1,13 +1,19 @@
 const router = require("express").Router()
 
 const userController = require("../Controllers/UserController")
+const testMiddleware = require("../middlewares/TestMiddleWare")
+const zodeMiddleware = require("../middlewares/ZodeMiddleWare")
+const userValidationSchema = require("../validationschemas/UserValidationSchemas")
+
 
 router.get("/users",userController.getAllUsers)
 router.get("/user/:id",userController.getUserById)
 router.get("/searchUser",userController.searchUser)
 router.delete("/user/:id",userController.deleteUser)
 
-router.post("/user",userController.createUser)
+// router.post("/user",userController.createUser)
+// router.post("/user",testMiddleware("MANAGER"),userController.createUser)
+router.post("/user",zodeMiddleware(userValidationSchema),userController.createUser)
 router.put("/user/:id",userController.updateUser)
 router.put("/userbyage/:age",userController.updateByAge)
 
